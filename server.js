7e809bb18c5e890a02e503d1646eccae677b9ce8
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2025-01-09 07:25:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Melayani file statis
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 app.get('/auth/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
